931cfba9f0d4eded2687af8526bec998a316c596
mabel <bigbelk@live.ru>
2022-09-14 23:42:06 +0300

    words

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,4 +1,8 @@
 import { writable } from 'svelte/store'
+import { PartsOfSpeech } from './parts-of-speech'
+
+console.log(Object.keys(PartsOfSpeech).filter(v => isNaN(Number(v))).map(key => key.toLowerCase()))
+
 
 const words = writable("Однажды в студеную зимнюю пору вах вах вах".split(' '))
 
