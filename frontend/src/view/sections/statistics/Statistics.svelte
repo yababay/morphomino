39d65f16e88c5775c09bb20397dcc13cc28a5f5e
mabel <bigbelk@live.ru>
2022-09-25 09:50:04 +0300

    statistics-pos

--- frontend/src/view/sections/statistics/Statistics.svelte
+++ frontend/src/view/sections/statistics/Statistics.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import Section from '../Section.svelte'
+    import PartsOfSpeech from './PartsOfSpeech.svelte';
 
     export let id
 </script>
 
+<PartsOfSpeech />
 <Section {id}>
     <svelte:fragment slot="dynamic">
         {id}
