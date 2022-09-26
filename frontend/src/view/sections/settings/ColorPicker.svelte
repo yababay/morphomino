8cb59629bf89c8b7e6a877ede37d5666238cf68e
mabel <bigbelk@live.ru>
2022-09-27 02:56:42 +0300

    settings-color

--- frontend/src/view/sections/settings/ColorPicker.svelte
+++ frontend/src/view/sections/settings/ColorPicker.svelte
@@ -1,32 +1,40 @@
 <script>
-        function changebackground(){
+        function changeStroke(e){
+            e.target.closest('section').querySelectorAll('svg').forEach(element => {
+                element.style.fill = 'red'
+            });
             //document.getElementById('result').style.backgroundColor = 'rgb(' + document.getElementById("OutputIdRed").value + ',' + document.getElementById("OutputIdGreen").value + ',' + document.getElementById("OutputIdBlue").value + ')';
         }
+
+        let redInput, greenInput, blueInput = 127
 </script>
 
-<!-- div class="container">
-    <div class="row">
-        <div class="col-md-4"></div>
-        <div id="result" class="shadow rounded col-md-4 d-flex justify-content-center p-5 my-5" style="background-color: rgb(127,127,127);">
-
-        </div>
-        <div class="col-md-4"></div>
-    </div>
+<div class="container">
     <div class="row">
         <div class="col-md-4 col-lg-4 p-5">
-            <p class="text-center text-white">RED</p>
-            <input type="range" class="form-range" min="0" max="255" step="1" id="customRange1" oninput='OutputIdRed.value = customRange1.value' onchange="changebackground()">
-            <output class="text-center text-white" style="display: block;" id="OutputIdRed">127</output>
+            <p class="text-center">Красный</p>
+            <input type="range" class="form-range" min="0" max="255" step="1" 
+                on:input={e => redInput = e.target.value} 
+                on:change={changeStroke}
+                value={127}>
         </div>
         <div class="col-md-4 col-lg-4 p-5">
-            <p class="text-center text-white">GREEN</p>
-            <input type="range" class="form-range" min="0" max="255" step="1" id="customRange2" oninput='OutputIdGreen.value = customRange2.value' onchange="changebackground()">
+            <p class="text-center">Зеленый</p>
+            <input type="range" class="form-range" min="0" max="255" step="1" on:input={e => greenInput = e.target.value} value={127}>
+        </div>
+        <div class="col-md-4 col-lg-4 p-5">
+            <p class="text-center">Синий</p>
+            <input type="range" class="form-range" min="0" max="255" step="1" on:input={e => blueInput = e.target.value} value={127}>
+        </div>
+        <!-- div class="col-md-4 col-lg-4 p-5">
+            <p class="text-center text-white">Зеленый</p>
+            <input type="range" class="form-range" min="0" max="255" step="1" id="customRange2" on:input='OutputIdGreen.value = customRange2.value' onchange="changebackground()">
             <output class="text-center text-white" style="display: block;" id="OutputIdGreen">127</output>
         </div>
         <div class="col-md-4 col-lg-4 p-5">
-            <p class="text-center text-white">BLUE</p>
+            <p class="text-center text-white">Синий</p>
             <input type="range" class="form-range" min="0" max="255" step="1" id="customRange3" oninput='OutputIdBlue.value = customRange3.value' onchange="changebackground()">
             <output class="text-center text-white" style="display: block;" id="OutputIdBlue">127</output>
-        </div>
+        </div -->
     </div>
-</div -->
+</div>
