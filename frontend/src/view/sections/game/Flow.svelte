26078ed09c87e0f5209bfdd7253450cf8c563903
mabel <bigbelk@live.ru>
2022-09-29 00:37:21 +0300

    audio-fix

--- frontend/src/view/sections/game/Flow.svelte
+++ frontend/src/view/sections/game/Flow.svelte
@@ -6,9 +6,9 @@
     let audioDeal
 </script>
 
-<audio bind:this={audioDeal}>
+<!-- audio bind:this={audioDeal}>
     <source src="./assets/audio/deal.mp3" type="audio/mpeg">
-</audio>
+</audio -->
 
 <div class="mormino-flow">
     <div class="flow-control">
@@ -17,7 +17,7 @@
     <div class="mormino-card">
         <MorminoCard word={$flow.at(-1).word} pos={$flow.at(-1).nextShortName} vignette={1} />
     </div>
-    <div class="flow-control" on:click={e => {audioDeal.play(); setRandomItems()}} style="cursor: pointer">
+    <div class="flow-control" on:click={e => {/*audioDeal.play();*/ setRandomItems()}} style="cursor: pointer">
         <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="grey" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
             <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
